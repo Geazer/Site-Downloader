06350a3f8e8c995cfc985b4d67cfc01d1f5d5505
Fedor Shmidt <aezur.zip@gmail.com>
2016-03-07 00:47:33 +0600

    Work in progress

--- sdown.py
+++ sdown.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import urllib.request
+import os
 #worked only with http:// or https://
 url = input("Please, enter the site address:")
 html = urllib.request.urlopen(url).read()
