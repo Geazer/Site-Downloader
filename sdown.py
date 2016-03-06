10213483a2477a7a33037688f41e2e2d80d7ebc7
Fedor Shmidt <aezur.zip@gmail.com>
2016-03-07 00:36:23 +0600

    Modified

--- sdown.py
+++ sdown.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
-import urllib
-
+import urllib.request
+#worked only with http:// or https://
 url = input("Please, enter the site address:")
-print(url)
+html = urllib.request.urlopen(url).read()
+
+f = open('index.html', 'wb')
+f.write(html)
