97a81bef2f16bd1267f405c68ba401ea3adbfd81
Fedor Shmidt <aezur.zip@gmail.com>
2016-03-07 15:56:10 +0600

    Modified

--- sdown.py
+++ sdown.py
@@ -1,10 +1,15 @@
 #!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 
 import urllib.request
-import os
-#worked only with http:// or https://
+import os.path
+
+#It works only with http:// or https://
 url = input("Please, enter the site address:")
 html = urllib.request.urlopen(url).read()
 
-f = open('index.html', 'wb')
+save_path = input("To save the file?:")
+
+completeName = os.path.join(save_path, "index.html")
+f = open(completeName, 'wb')
 f.write(html)
